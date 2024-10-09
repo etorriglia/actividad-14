5989528897e8dfd94793c7b65a81a6090f731965
etorriglia <etorrigliazimmer@gmail.com>
2024-10-08 22:31:18 -0300

    entrega

--- buscador_de_img_nasa entrega/buscador_de_img_nasa/js/space.js
+++ buscador_de_img_nasa entrega/buscador_de_img_nasa/js/space.js
@@ -1,6 +1,5 @@
-const searchInput = document.querySelector('.form-control');
 
-const clearIcon = document.querySelector('.clear-icon');
+const searchInput = document.querySelector('.search-input');
 
   let getJSONData = function(url){
       let result = {};
@@ -23,45 +22,100 @@ const clearIcon = document.querySelector('.clear-icon');
           return result;
       });
   }
+
+
+  searchInput.addEventListener('input', function () {
+    onSearchQueryChange(this.value);
+    loadData(NASA_URL);
+  });
   
-NASA_URL= "http://images-api.nasa.gov/search?q=null"
+
+  
+  function onSearchQueryChange(query) {
+    let searchQuery = query;
+    localStorage.setItem("search-query", searchQuery);
 
 
-  getJSONData(NASA_URL).then(function (resultObj) {
+    
+
+  }
+
+let NASA_URL='';
+let searchQuery='*'
+let imageArray = [];
+
+
+
+
+function loadData(dataURL){
+  searchQuery= localStorage.getItem("search-query");
+  NASA_URL= `http://images-api.nasa.gov/search?q=${searchQuery}`
+    getJSONData(NASA_URL).then(function (resultObj) {
     if (resultObj.status === "ok") {
       rawResults = resultObj.data;
-
+      imageArray=constructImageArray(rawResults);
+      run();
 
     }
-  });
+  })
+}
 
 
 
-searchInput.addEventListener('input', function () {
-  onSearchQueryChange(this.value);
-});
+function constructImageArray(rawResults){
+  imageArray= rawResults.collection.items;
+  return(imageArray);
 
+}
 
-function onSearchQueryChange(query) {
-  searchQuery = query;
-  filterImages(rawResults);
+function shortenDescription(descript){
+  if(descript.length>200){
+    descript= descript.slice(0,200) + "...";
   
-
+  }
+  return (descript);
 }
 
-let imagesInfoArray= [];
-
-function filterImages(){
-    imagesInfo= rawResults.collection;
-    console.log(imagesInfo)
-    imageInfoArray= [];
-    for (i=0; i<imagesInfo.length; i++){
-        imageInfoArray[i]=imagesInfo[i].data;
+function showImages(imageArray){
+  
+  searchResults= document.getElementById('search-results');
+  let searchResultsHTML='';
+    for (i=0; i<imageArray.length; i++){
+      if (imageArray[i].data[0].media_type=== "image"){
+        imageLink= imageArray[i].links[0]?.href;
+        imageTitle= imageArray[i].data[0]?.title;
+        imageDescription= shortenDescription(imageArray[i].data[0]?.description);
+        imageDate= imageArray[i].data[0]?.date_created;
+        searchResultsHTML+=
+        `<div class="card mb-12";">
+            <div class="row">
+              <div class="col-md-4">
+                <img src="${imageLink}" class="img-fluid" alt="${imageTitle}">
+              </div>
+              <div class="col-md-6">
+                <div class="card-body">
+                  <h5 class="card-title">${imageTitle}</h5>
+                  <p class="card-text">${imageDescription}.</p>
+                  <p class="card-date"><small class="text-muted">${imageDate}</small></p>
+                </div>
+              </div>
+            </div>
+          </div>
+          `
+    
     }
-    console.log(imagesInfoArray)
+  searchResults.innerHTML= searchResultsHTML;
 }
+}
+      
 
-
+async function run() {
+  try {
+    await showImages(imageArray);
+  } catch (err) {
+    console.error('error occured: ', err.message)
+  }
+}
 
 
 /*
